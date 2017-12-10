49fadda927c5d5cf9568dee85b902d44eb9e65f8
minchao <minchao.220@gmail.com>
2017-12-10 19:49:59 +0800

    Add Client

---
+++ epa.go
@@ -0,0 +1,132 @@
+package epa
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+const (
+	libraryVersion   = "0.0.1"
+	defaultBaseURL   = "https://opendata.epa.gov.tw/"
+	defaultUserAgent = "go-epa/" + libraryVersion
+)
+
+// A Client manages communication with the EPA Open data API.
+type Client struct {
+	client *http.Client
+	token  string
+
+	BaseURL   *url.URL
+	UserAgent string
+}
+
+// NewClient returns a new EPA Open data API client.
+func NewClient(token string, httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	baseURL, _ := url.Parse(defaultBaseURL)
+
+	return &Client{
+		client:    httpClient,
+		token:     token,
+		UserAgent: defaultUserAgent,
+		BaseURL:   baseURL,
+	}
+}
+
+// NewRequest creates an API request.
+func (c *Client) NewRequest(method, urlStr string, body io.Reader) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	return req, nil
+}
+
+// Do sends an API request and returns the API response.
+//
+// The provided ctx must be non-nil. If it is canceled or time out, ctx.Err() will be returned.
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	req = req.WithContext(ctx)
+
+	q := req.URL.Query()
+	q.Set("token", c.token)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+
+		// If the error type is *url.Error, sanitize its URL before returning.
+		if e, ok := err.(*url.Error); ok {
+			if u, err := url.Parse(e.URL); err == nil {
+				e.URL = sanitizeURL(u).String()
+				return nil, e
+			}
+		}
+
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if err := CheckResponse(resp); err != nil {
+		return resp, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+			if err == io.EOF {
+				err = nil // ignore EOF errors caused by empty response body
+			}
+		}
+	}
+
+	return resp, nil
+}
+
+// sanitizeURL redacts the PWD parameter from the URL which may be exposed to the user.
+func sanitizeURL(uri *url.URL) *url.URL {
+	if uri == nil {
+		return nil
+	}
+	params := uri.Query()
+	if len(params.Get("token")) > 0 {
+		params.Set("token", "REDACTED")
+		uri.RawQuery = params.Encode()
+	}
+	return uri
+}
+
+// CheckResponse checks the API response for errors.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+
+	// TODO
+
+	return nil
+}
---
+++ epa_test.go
@@ -0,0 +1,77 @@
+package epa
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+var (
+	// mux is the HTTP request multiplexer used with the test server.
+	mux *http.ServeMux
+
+	// client is the Apple Music client being tested.
+	client *Client
+
+	// server is a test HTTP server used to provide mock API responses.
+	server *httptest.Server
+)
+
+// setup sets up a test HTTP server along with a every9d.Client that is configured to talk to that test server.
+func setup() {
+	// test server
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	// EVERY8D client configured to use test server
+	client = NewClient("token", nil)
+	u, _ := url.Parse(server.URL)
+	client.BaseURL = u
+}
+
+// teardown closes the test HTTP server.
+func teardown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	if got := r.Method; got != want {
+		t.Errorf("Request method is %v, want %v", got, want)
+	}
+}
+
+type values map[string]string
+
+func testFormValues(t *testing.T, r *http.Request, values values) {
+	want := url.Values{}
+	for k, v := range values {
+		want.Add(k, v)
+	}
+
+	r.ParseForm()
+	r.Form.Del("token") // Remove token
+	if got := r.Form; !reflect.DeepEqual(got, want) {
+		t.Errorf("Request parameters is %v, want %v", got, want)
+	}
+}
+
+func areEqualJSON(j1, j2 []byte) (bool, error) {
+	var v1 interface{}
+	var v2 interface{}
+
+	var err error
+	err = json.Unmarshal(j1, &v1)
+	if err != nil {
+		return false, fmt.Errorf("Unmarshal JSON 1 error: %v", err)
+	}
+	err = json.Unmarshal(j2, &v2)
+	if err != nil {
+		return false, fmt.Errorf("Unmarshal JSON 2 error: %v", err)
+	}
+
+	return reflect.DeepEqual(v1, v2), nil
+}
